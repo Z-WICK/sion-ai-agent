cdc48ad3757cffaba1f52cb56874e59f31cc9221
Z-WICK <2289431216@qq.com>
2025-06-19 17:45:30 +0800

    feat: rag 支持文本重写

---
+++ src/main/java/com/sion/sionaiagent/rag/QueryRewriter.java
@@ -0,0 +1,39 @@
+package com.sion.sionaiagent.rag;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.rag.Query;
+import org.springframework.ai.rag.preretrieval.query.transformation.QueryTransformer;
+import org.springframework.ai.rag.preretrieval.query.transformation.RewriteQueryTransformer;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author : wick
+ * @Date : 2025/6/19 16:40
+ */
+@Component
+public class QueryRewriter {
+
+    private final QueryTransformer queryTransformer;
+
+    public QueryRewriter(ChatModel dashscopeChatModel) {
+        ChatClient.Builder builder = ChatClient.builder(dashscopeChatModel);
+
+        // 创建查询重写转换器
+        queryTransformer = RewriteQueryTransformer.builder()
+                .chatClientBuilder(builder)
+                .build();
+
+    }
+
+
+    public String doQueryRewrite(String prompt){
+        Query query = new Query(prompt);
+
+        // 执行查询重写
+        Query transformQuery = queryTransformer.transform(query);
+
+        // 输出重写后的查询
+        return transformQuery.text();
+    }
+}

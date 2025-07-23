6bda5757e78972b0d7cc219aa872e3873c1204fb
Z-WICK <2289431216@qq.com>
2025-07-23 15:38:34 +0800

    feat: mcp服务开发-图片搜索

---
+++ sion-image-search-mcp-server/src/test/java/com/sion/sionimagesearchmcp/SionImageSearchMcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sion.sionimagesearchmcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SionImageSearchMcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- src/test/java/com/sion/sionaiagent/app/LoveAppTest.java
+++ src/test/java/com/sion/sionaiagent/app/LoveAppTest.java
@@ -78,11 +78,21 @@ class LoveAppTest {
     }
 
     @Test
-    void doChatWithMcp() {
+    void doChatWithMcpGaoDe() {
         String chatId = UUID.randomUUID().toString();
         // 测试地图 MCP
         String message = "我的另一半居住在广东湛江盛产菠萝的地方，请帮我找到5公里内合适的约会地点";
         String answer = loveApp.doChatWithMcp(message, chatId);
+    }
+
+    @Test
+    void doChatWithMcp() {
+        String chatId = UUID.randomUUID().toString();
+
+        // 测试图片搜索 MCP
+        String message = "帮我搜索一些哄另一半开心的图片";
+        String answer = loveApp.doChatWithMcp(message,chatId);
+        Assertions.assertNotNull(answer);
     }
 
 }
